04cf544c286dabac4feb7272cdfe1a9fd107f384
Phan Anh Khang <phananhkhang0603@gmail.com>
2026-05-13 23:35:55 +0700

    Khời tạo

---
+++ src/main/java/org/akira/auratech/AuraTechApplication.java
@@ -0,0 +1,13 @@
+package org.akira.auratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraTechApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(AuraTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/akira/auratech/AuraTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akira.auratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
