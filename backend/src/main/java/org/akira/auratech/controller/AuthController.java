64e40577ceb08430e24983477ebe96a6ba78d72c
Phan Anh Khang <phananhkhang0603@gmail.com>
2026-06-04 14:19:56 +0700

    chore: docs

--- backend/src/main/java/org/akira/auratech/controller/AuthController.java
+++ backend/src/main/java/org/akira/auratech/controller/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
